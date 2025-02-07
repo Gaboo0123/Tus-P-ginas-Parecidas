9f297e2d46475232c910356c255abf657f659c2d
Gaboo0123 <gabrielheresicueto@gmail.com>
2025-02-07 10:35:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,30 @@ import random
 # Base de datos de marcas y nombres similares
 SIMILAR_NAMES = {
     "Nike": ["Adidas", "Puma", "Reebok", "Under Armour"],
-     "Pepsi": ["Adidas", "Puma", "Reebok", "Under Armour"],
+     "Adidas": ["Nike", "Puma", "Reebok", "Under Armour"],
+     "Puma": ["Adidas", "Nike", "Reebok", "Under Armour"],
+     "Reebok": ["Adidas", "Puma", "Nike", "Under Armour"],
+     "Under Armour": ["Adidas", "Puma", "Reebok", "Nike"],
     "Coca-Cola": ["Pepsi", "Dr Pepper", "Fanta", "Sprite"],
+    "Pepsi": ["Coca-Cola", "Dr Pepper", "Fanta", "Sprite"],
+    "Dr Pepper": ["Pepsi", "Coca-Cola", "Fanta", "Sprite"],
+    "Fanta": ["Pepsi", "Dr Pepper", "Coca-Cola", "Sprite"],
+    "Sprite": ["Pepsi", "Dr Pepper", "Fanta", "Sprite"],
     "Apple": ["Samsung", "Google", "Huawei", "Microsoft"],
-    "McDonald's": ["Burger King", "Wendy's", "KFC", "Taco Bell"],
-    "Ford": ["Chevrolet", "Toyota", "Honda", "Nissan"]
+    "Samsung": ["Apple", "Google", "Huawei", "Microsoft"],
+    "Google": ["Samsung", "Apple", "Huawei", "Microsoft"],
+    "Huawei": ["Samsung", "Google", "Apple", "Microsoft"],
+    "Microsoft": ["Samsung", "Google", "Huawei", "Apple"],
+    "Burger King": ["McDonalds", "Wendy's", "KFC", "Popeyes"],
+    "Wendys": ["Burger King", "McDonalds", "KFC", "Popeyes"],
+    "KFC": ["Burger King", "Wendy's", "McDonalds", "Popeyes"],
+    "Popeyes": ["Burger King", "Wendy's", "KFC", "McDonalds"],
+    "McDonald's": ["Burger King", "Wendy's", "KFC", "Popeyes"],
+    "Ford": ["Subaru", "Toyota", "Honda", "Nissan"]
+    "Subaru": ["Ford", "Toyota", "Honda", "Nissan"]
+    "Toyota": ["Subaru", "Ford", "Honda", "Nissan"]
+    "Honda": ["Subaru", "Toyota", "Ford", "Nissan"]
+    "Nissan": ["Subaru", "Toyota", "Honda", "Ford"]
 }
 
 def get_similar_names(name):
